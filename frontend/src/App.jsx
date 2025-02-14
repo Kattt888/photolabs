e42061862841ce02a13f3ad12b7093afbe08f3a9
Katrina <katrinabyrd88@gmail.com>
2025-02-13 21:54:11 -0500

    created a component container for all the components. Adjusted the scss to ensure it looked closer to the example photo of the website.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
-function App() {
-const photos = [
-{
-  id: "1",
-  username: "Joe Example",
-  location: { city: "Montreal", country: "Canada" },
-  imageSource: "/Image-1-Regular.jpeg",
-  profile: "/profile-1.jpg",
-},
-{
-  id: "2",
-  username: "Kevin Costner",
-  location: { city: "Toronto", country: "Canada" },
-  imageSource: "/Image-1-Regular.jpeg",
-  profile: "/profile-1.jpg",
-},
-{
-  id: "3",
-  username: "Eartha Kitt",
-  location: { city: "Alabama", country: "USA" },
-  imageSource: "/Image-1-Regular.jpeg",
-  profile: "/profile-1.jpg",
-}
-];
-
-return (
-    <div className="photo-list">
-    {photos.map((photo) => (
-      <PhotoListItem
-      key={photo.id}
-      photo={photo}
-      />
-     ))}
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <PhotoList />
+  </div>
+);
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import PhotoFavButton from './PhotoFavButton';
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photo }) => {
-  const { id, username, location, imageSource, profile } = photo;
+  if (!photo) return null;
+  const { id, location, urls, user } = photo;
 
   return (
     <div className="photo-list__item">
+      <div className="photo-list__image-container">
+      <PhotoFavButton />
       <img
-        src={imageSource}
-        alt={`Photo by ${username}`}
+        src={urls.regular}
+        alt={`Photo by ${user.username}`}
         className="photo-list__image"
       />
-      <p>Photographer: {username}</p>
+      </div>
+
+      <p>Photographer: {user.username}</p>
       <p>Location: {location.city}, {location.country}</p>
+      
       <img
-        src={profile}
-        alt={`${username}'s Profile Picture`}
+        src={user.profile}
+        alt={`${user.username}'s Profile Picture`}
         className="photo-list__profile"
       />
     </div>
