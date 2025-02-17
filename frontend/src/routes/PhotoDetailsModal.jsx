846216ddc7acd8d0284a492bda5c80073b626bbb
Katrina <katrinabyrd88@gmail.com>
2025-02-17 00:30:11 -0500

    create a blank modal on the website

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,30 @@
+import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ selectedPhoto, closeModal }) => {
+if (!selectedPhoto) return null;
+
+const { urls, user, location } = selectedPhoto;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+<div className="photo-details-modal__content">
+  <img
+  src={urls?.regular || "/placeholder-image.jpg"}
+  alt={`Photo by ${user?.username || "Unknown"}`}
+  className="photo-details-modal__image"
+  />
+  <div className="photo-details-modal__info">
+          <p><strong>Photographer:</strong> {user?.username || "Unknown"}</p>
+          <p><strong>Location:</strong> {location?.city || "Unknown City"}, {location?.country || "Unknown Country"}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
