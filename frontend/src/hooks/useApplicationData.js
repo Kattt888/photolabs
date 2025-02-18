abba5e542d83573bfd856171da63189e4030f46a
Katrina <katrinabyrd88@gmail.com>
2025-02-18 12:35:07 -0500

    separated concerns, created useApplicationData.js file as the custom hook responsible for loading initial data from API. created directory for hooks. Adjusted the app.jsx file.

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import photos from "./mocks/photos";
+
+const useApplicationData = () => {
+  // Defining State
+  const [state, setState] = useState({
+    favPhotos: [],
+    selectedPhoto: null,
+    displayModal: false,
+  });
+
+  // Update Favorite Photos
+  const updateToFavPhotoIds = (photoId) => {
+    setState((prev) => ({
+      ...prev,
+      favPhotos: prev.favPhotos.includes(photoId)
+        ? prev.favPhotos.filter((id) => id !== photoId)
+        : [...prev.favPhotos, photoId],
+    }));
+  };
+
+  // Set Selected Photo (Open Modal)
+  const setPhotoSelected = (photo) => {
+    setState((prev) => ({
+      ...prev,
+      selectedPhoto: photo,
+      displayModal: true,
+    }));
+  };
+
+  // Close Modal
+  const onClosePhotoDetailsModal = () => {
+    setState((prev) => ({
+      ...prev,
+      selectedPhoto: null,
+      displayModal: false,
+    }));
+  };
+
+  // Return State & Actions
+  return {
+    state,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal,
+  };
+};
+
+export default useApplicationData;
